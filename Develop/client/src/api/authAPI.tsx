ce59d0ea6e441242f1cd0937b646df38de88af06
KyBrMar12 <KyBrMar@gmail.com>
2025-02-04 21:05:37 -0600

    Fetch and redirect code for login

--- Develop/client/src/api/authAPI.tsx
+++ Develop/client/src/api/authAPI.tsx
@@ -1,9 +1,27 @@
 import { UserLogin } from "../interfaces/UserLogin";
 
 const login = async (userInfo: UserLogin) => {
-  // TODO: make a POST request to the login route
-}
+  try {
+    console.log('🔍 Sending Login Request:', userInfo);
 
+    const response = await fetch('http://localhost:3001/auth/login', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(userInfo),
+    });
 
+    if (!response.ok) {
+      const errorMessage = await response.json();
+      throw new Error(`Login failed: ${errorMessage.message}`);
+    }
+
+    const data = await response.json();
+    console.log('✅ Login Success:', data);
+    return data;
+  } catch (err) {
+    console.error('❌ Login error:', err);
+    return Promise.reject('Login failed');
+  }
+};
 
 export { login };
